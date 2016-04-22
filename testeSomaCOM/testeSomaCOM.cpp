6cf4754c3ab245004d37feb28f05a32d35ba81a0
Fábio da Silva Santana <fabio.ssanana.dev@gmail.com>
2016-04-22 16:55:21 -0300

    first commit

--- testeSomaCOM/testeSomaCOM.cpp
+++ testeSomaCOM/testeSomaCOM.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
-
+#include <Windows.h>
+#include <atlbase.h>
+#include "SomaCOM_i.h"
+#include "SomaCOM_i.c"
+#include <iostream>
 
 int main()
 {
+	HRESULT hr;
+
+	hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+
+	CComPtr<ISoma> pSoma;
+
+	hr = pSoma.CoCreateInstance(CLSID_Soma);
+
+	if (FAILED(hr)) {
+		return 1;
+	}
+
+	hr = pSoma->setPrimeiroValor(2);
+
+	hr = pSoma->setSegundoValor(2);
+
+	int ret;
+
+	hr = pSoma->getSoma(&ret);
+	std::cout << ret << std::endl;
     return 0;
 }
 
